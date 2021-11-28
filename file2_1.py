fc900a120b79218c2c639cf49dc22ca528e9499d
Vladyslav <V.Koshchavka_FIT_8_21_B_d@knute.edu.ua>
2021-11-29 00:18:11 +0300

    /

---
+++ file2_1.py
@@ -0,0 +1,21 @@
+from array import *
+import pylab
+
+text = input("yourtext")
+
+letters_str='abcdefghijklmnopqrstuvwxyz'
+letters=list(letters_str)
+frequency=[]
+
+L=len(letters)
+
+for i in range(L):
+    elem=letters[i]
+    fr=text.count(elem)
+    frequency.append(fr)
+xdata=letters
+ydata=frequency
+pylab.bar (xdata, ydata)
+pylab.savefig('2.png', dpi=200)
+
+pylab.show()
